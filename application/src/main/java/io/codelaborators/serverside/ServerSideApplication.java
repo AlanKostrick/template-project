26edbd0b098f4dfab3d48648167836a74457e6a9
AlanKostrick <alankostrick@gmail.com>
2022-07-31 13:15:36 -0400

    Initial commit

--- application/src/main/java/io/codelaborators/serverside/ServerSideApplication.java
+++ application/src/main/java/io/codelaborators/serverside/ServerSideApplication.java
@@ -1,4 +1,4 @@
-package org.wecancodeit.serverside;
+package io.codelaborators.serverside;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
